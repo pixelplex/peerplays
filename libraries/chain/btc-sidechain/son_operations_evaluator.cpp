321c7ba4c51348ed1e18c1204b38f0adb9899ada
Pixelplex Account <dev@pixelplex.by>
2019-07-22 09:36:58 +0300

    Added son deposit functional

--- libraries/chain/btc-sidechain/son_operations_evaluator.cpp
+++ libraries/chain/btc-sidechain/son_operations_evaluator.cpp
@@ -7,7 +7,8 @@ namespace graphene { namespace chain {
 
 void_result create_son_member_evaluator::do_evaluate(const son_member_create_operation& op)
 { try{
-    FC_ASSERT(db().get(op.owner_account).is_lifetime_member(), "Only Lifetime members may register a SON.");
+    FC_ASSERT(db().get_balance(op.owner_account, asset_id_type()).amount >= db().get_global_properties().parameters.son_deposit_amount,
+              "Not enought core asset to create SON." );
     return void_result();
 } FC_CAPTURE_AND_RETHROW( (op) ) }
 
@@ -21,8 +22,12 @@ object_id_type create_son_member_evaluator::do_apply(const son_member_create_ope
     const auto& new_son_object = db().create<son_member_object>( [&]( son_member_object& obj ){
         obj.son_member_account = op.owner_account;
         obj.vote_id            = vote_id;
+        obj.deposit            = db().get_global_properties().parameters.son_deposit_amount;
         obj.url                = op.url;
     });
+
+    db().adjust_balance( op.owner_account, -asset( db().get_global_properties().parameters.son_deposit_amount, asset_id_type() ) );
+
     return new_son_object.id;
 } FC_CAPTURE_AND_RETHROW( (op) ) }
 
@@ -30,14 +35,18 @@ void_result delete_son_member_evaluator::do_evaluate(const son_member_delete_ope
 { try {
     database& d = db();
     const auto& idx = d.get_index_type<son_member_index>().indices().get<by_account>();
-    FC_ASSERT( idx.find(op.owner_account) != idx.end() );    
+    auto find_iter = idx.find(op.owner_account);
+    FC_ASSERT( find_iter != idx.end(), "No SON registed for this account." );
+    FC_ASSERT( !find_iter->time_of_deleting, "SON already on deleting stage." );
     return void_result();
 } FC_CAPTURE_AND_RETHROW( (op) ) }
 
 void_result delete_son_member_evaluator::do_apply(const son_member_delete_operation& op)
 { try {
     const auto& idx = db().get_index_type<son_member_index>().indices().get<by_account>();
-    db().remove(*idx.find(op.owner_account));
+    db().modify( *idx.find(op.owner_account), [this]( son_member_object& o ) {
+        o.time_of_deleting = db().head_block_time();
+    });
     return void_result();
 } FC_CAPTURE_AND_RETHROW( (op) ) }
 
